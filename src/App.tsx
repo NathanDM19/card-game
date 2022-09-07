9435ec8bd1cd2df44a1135822b36a1edcbd48211
NathanDM19 <creationmatee@gmail.com>
2022-09-07 20:57:27 +1000

    Card game

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,147 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect, useRef } from 'react';
 import './App.css';
+import cardImages, { back } from "./cards/cards";
+
+interface Card {
+  image: string;
+  flipped: boolean;
+  matched: boolean;
+}
 
 function App() {
+  const [totalCards, setTotalCards] = useState(36);
+  const [cards, setCards] = useState<Card[]>([]);
+  const [flippedCards, setFlippedCards] = useState<number[]>([]);
+  const [guesses, setGuesses] = useState<number[]>([]);
+  const [paused, setPaused] = useState(false);
+  const [timer, setTimer]: any = useState(0);
+  const [active, setActive] = useState(false);
+  const [won, setWon] = useState(false);
+  let intervalRef = useRef(timer);
+  
+  useEffect(() => placeCards(totalCards), []);
+
+  function placeCards(cardsUsed: number) {
+    clearInterval(intervalRef.current);
+    setActive(false);
+    setTimer(0);
+    setWon(false);
+    setFlippedCards([]);
+    const newCards: Card[] = [];
+    const cardsArray = [...cardImages.slice(0, cardsUsed / 2), ...cardImages.slice(0, cardsUsed / 2)];
+    while (cardsArray.length) {
+      const random = Math.floor(Math.random() * cardsArray.length);
+      newCards.push({image: cardsArray[random], flipped: false, matched: false});
+      cardsArray.splice(random, 1);
+    }
+    setCards(newCards);
+  }
+
+  function clickCard(i: number) {
+    if (!cards[i].matched && !paused && !flippedCards.includes(i)) {
+      if (!active) {
+        setActive(true);
+        intervalRef.current = setInterval(() => setTimer((timer: number) => timer + .1), 100);
+      }
+      let tempCards: Card[] = [...cards];
+      tempCards[i].flipped = true;
+      setCards(tempCards);
+      flippedCards.push(i);
+      if (flippedCards.length === 2) {
+        checkCards();
+      }
+    }
+  }
+
+  function checkCards() {
+    setPaused(true);
+    const selectedCards: Card[] = [];
+    const places: number[] = [];
+    for (let i = 0; i < cards.length; i++) {
+      if (cards[i].flipped) {
+        selectedCards.push(cards[i]);
+        places.push(i);
+      }
+    }
+    const tempCards = [...cards];
+    if (selectedCards[0].image === selectedCards[1].image) {
+      tempCards[places[0]].matched = true;
+      tempCards[places[1]].matched = true;
+      finishGo(tempCards, places);
+    } else {
+      if (guesses.includes(places[0]) || guesses.includes(places[1])) {
+        setTimer((timer: number) => timer + 5);
+      }
+      setTimeout(() => {
+        finishGo(tempCards, places);
+      }, 500);
+    }
+    setFlippedCards([]);
+    checkWin();
+  }
+
+  function finishGo(tempCards: Card[], places: number[]) {
+    tempCards[places[0]].flipped = false;
+    tempCards[places[1]].flipped = false;
+    setGuesses([...guesses, places[0], places[1]]);
+    setCards(cards);
+    setPaused(false);
+  }
+
+  function checkWin() {
+    let won: boolean = true;
+    for (let i = 0; i < cards.length; i++) {
+      if (!cards[i].matched) {
+        won = false;
+      }
+    }
+    if (won) {
+      clearInterval(intervalRef.current);
+      setWon(true);
+    }
+  }
+
+  function changeTotalCards(total: number) {
+    setTotalCards(total);
+    placeCards(total);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="cards">
+        <div className="selectCards">
+          <label>Total Cards: </label>
+          <select 
+            name="cards" 
+            id="cards" 
+            defaultValue={totalCards}
+            onChange={(e) => changeTotalCards(parseInt(e.target.value))}>
+            {Array(51).fill("").map((_, i) => {
+              const total = i * 2 + 4;
+              return (
+                <option key={i} value={`${total}`}>{total}</option>
+              )
+            })}
+          </select>
+        </div>
+        <p className="timer">Timer: {timer === 0 ? 0 : timer.toFixed(1)}</p>
+        <button className="reset" onClick={() => placeCards(totalCards)}>Reset</button>
+        {/* <br/><br/><br/> */}
+        {cards.map((card, i) => {
+          return (
+              <img 
+                key={`card${i}`} 
+                className="card"
+                onClick={() => clickCard(i)}
+                src={card.flipped || card.matched ? card.image: back} />
+          )
+        })}
+      {won && <div className="won">
+        <h1>Congratulations!</h1>
+        <p>You finished the game in {timer.toFixed(1)} seconds</p>
+        <button onClick={() => placeCards(totalCards)}>Reset</button>
+        </div>}
+      </div>
     </div>
   );
 }
---
+++ src/cards/cards.tsx
@@ -0,0 +1,109 @@
+import card1 from "./ace_of_clubs.png";
+import card2 from "./ace_of_diamonds.png";
+import card3 from "./ace_of_hearts.png";
+import card4 from "./ace_of_spades.png";
+import card5 from "./2_of_clubs.png";
+import card6 from "./2_of_diamonds.png";
+import card7 from "./2_of_hearts.png";
+import card8 from "./2_of_spades.png";
+import card9 from "./3_of_clubs.png";
+import card10 from "./3_of_diamonds.png";
+import card11 from "./3_of_hearts.png";
+import card12 from "./3_of_spades.png";
+import card13 from "./4_of_clubs.png";
+import card14 from "./4_of_diamonds.png";
+import card15 from "./4_of_hearts.png";
+import card16 from "./4_of_spades.png";
+import card17 from "./5_of_clubs.png";
+import card18 from "./5_of_diamonds.png";
+import card19 from "./5_of_hearts.png";
+import card20 from "./5_of_spades.png";
+import card21 from "./6_of_clubs.png";
+import card22 from "./6_of_diamonds.png";
+import card23 from "./6_of_hearts.png";
+import card24 from "./6_of_spades.png";
+import card25 from "./7_of_clubs.png";
+import card26 from "./7_of_diamonds.png";
+import card27 from "./7_of_hearts.png";
+import card28 from "./7_of_spades.png";
+import card29 from "./8_of_clubs.png";
+import card30 from "./8_of_diamonds.png";
+import card31 from "./8_of_hearts.png";
+import card32 from "./8_of_spades.png";
+import card33 from "./9_of_clubs.png";
+import card34 from "./9_of_diamonds.png";
+import card35 from "./9_of_hearts.png";
+import card36 from "./9_of_spades.png";
+import card37 from "./10_of_clubs.png";
+import card38 from "./10_of_diamonds.png";
+import card39 from "./10_of_hearts.png";
+import card40 from "./10_of_spades.png";
+import card41 from "./jack_of_clubs.png";
+import card42 from "./jack_of_diamonds.png";
+import card43 from "./jack_of_hearts.png";
+import card44 from "./jack_of_spades.png";
+import card45 from "./queen_of_clubs.png";
+import card46 from "./queen_of_diamonds.png";
+import card47 from "./queen_of_hearts.png";
+import card48 from "./queen_of_spades.png";
+import card49 from "./king_of_clubs.png";
+import card50 from "./king_of_diamonds.png";
+import card51 from "./king_of_hearts.png";
+import card52 from "./king_of_spades.png";
+import back from "./back.png";
+
+export { back };
+
+export default [card1,
+     card2,
+     card3,
+     card4,
+     card5,
+     card6,
+     card7,
+     card8,
+     card9,
+     card10,
+     card11,
+     card12,
+     card13,
+     card14,
+     card15,
+     card16,
+     card17,
+     card18,
+     card19,
+     card20,
+     card21,
+     card22,
+     card23,
+     card24,
+     card25,
+     card26,
+     card27,
+     card28,
+     card29,
+     card30,
+     card31,
+     card32,
+     card33,
+     card34,
+     card35,
+     card36,
+     card37,
+     card38,
+     card39,
+     card40,
+     card41,
+     card42,
+     card43,
+     card44,
+     card45,
+     card46,
+     card47,
+     card48,
+     card49,
+     card50,
+     card51,
+     card52,
+];
